ae00b528c07c93fdc4f7b15531107fa465980f67
Jowell Villasin <jowell.villasin@gmail.com>
2015-05-13 09:14:57 -0700

    add basic strat and token res

--- server.js
+++ server.js
@@ -2,15 +2,25 @@
 
 var mongoose = require('mongoose');
 var express = require('express');
+var passport = require('passport');
 var app = express();
 
-var moviesRoutes = express.Router();
+process.env.APP_SECRET = process.env.APP_SECRET || 'changethischangethischangethis';
 
-mongoose.connect(process.env.MONGOLAB_URI ||'mongodb://localhost/dev_db');
+var moviesRoutes = express.Router();
+var usersRoutes = express.Router();
+
+mongoose.connect(process.env.MONGOLAB_URI ||'mongodb://localhost/movies_dev');
+
+app.use(passport.initialize());
+
+require('./lib/passport_strat')(passport);
 
 require('./routes/movies_routes')(moviesRoutes);
+require('./routes/auth_routes')(usersRoutes, passport);
 
 app.use('/api', moviesRoutes);
+app.use('/api', usersRoutes);
 
 app.listen(process.env.PORT || 3000, function() {
   console.log('server up on port ' + (process.env.PORT || 3000));
